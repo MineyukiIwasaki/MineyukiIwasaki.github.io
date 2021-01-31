6bf06f5b5d4b38d260d66cd5f8560e2866dec129
Mineyuki Iwasaki <mineyuki.iwasaki@gmail.com>
2021-01-31 18:29:52 +0300

    [Image, css, js] Added images, css and js.

---
+++ docs/js/javascript.js
@@ -0,0 +1,22 @@
+window.addEventListener("scroll",function(){
+let header=document.getElementById('header');
+if(header){
+if(window.pageYOffset<50){
+header.style.backgroundColor='rgba(0,0,0,0)';
+}else{
+header.style.backgroundColor='rgba(0,0,0,0.75)';
+}
+}
+let parallax=document.getElementById('parallax');
+let parallax2=document.getElementById('parallax2');
+let parallax3=document.getElementById('parallax3');
+if(parallax){
+parallax.style.backgroundPositionY=String(-0.5*window.pageYOffset)+'px';
+}
+if(parallax2){
+parallax2.style.backgroundPositionY=String(-0.5*window.pageYOffset+500)+'px';
+}
+if(parallax3){
+parallax3.style.backgroundPositionY=String(-0.5*window.pageYOffset+900)+'px';
+}
+});
