0dad59bf9437d026f09818cc36acccaa1613f47f
Mineyuki Iwasaki <mineyuki.iwasaki@gmail.com>
2021-02-01 22:09:07 +0300

    [Test] Investigate parallax.

--- docs/js/javascript.js
+++ docs/js/javascript.js
@@ -8,7 +8,15 @@ header.style.backgroundColor='rgba(0,0,0,0.75)';
 }
 }
 let parallax=document.getElementById('parallax');
+let parallax2=document.getElementById('parallax2');
+let parallax3=document.getElementById('parallax3');
 if(parallax){
-parallax.style.backgroundPositionY=String(0.5*window.pageYOffset)+'px';
+parallax.style.backgroundPositionY=String(-0.5*window.pageYOffset-160)+'px';
+}
+if(parallax2){
+parallax2.style.backgroundPositionY=String(-0.5*window.pageYOffset+370-100)+'px';
+}
+if(parallax3){
+parallax3.style.backgroundPositionY=String(-0.5*window.pageYOffset+760-100)+'px';
 }
 });
