ee710980e2682262295ef9184165de34460e387d
Mineyuki Iwasaki <mineyuki.iwasaki@gmail.com>
2021-02-02 11:02:07 +0300

    [Test] Fixed parallax issue for smart phone.

--- docs/js/javascript.js
+++ docs/js/javascript.js
@@ -8,31 +8,24 @@ header.style.backgroundColor='rgba(0,0,0,0.75)';
 }
 }
 let user_agent=window.navigator.userAgent.toLowerCase();
-if(user_agent.indexOf("android")!==-1 || user_agent.indexOf("ipad")!==-1 || user_agent.indexOf("iphone")!==-1){
 let parallax=document.getElementById('parallax');
 let parallax2=document.getElementById('parallax2');
 let parallax3=document.getElementById('parallax3');
+if(user_agent.indexOf("android")!==-1 || user_agent.indexOf("ipad")!==-1 || user_agent.indexOf("iphone")!==-1){
 if(parallax){
-parallax.style.backgroundAttachment='scroll';
 parallax.style.backgroundPositionY=String(0.5*window.pageYOffset)+'px';
 }
-if(parallax2){
-parallax2.style.backgroundAttachment='scroll';
-}
-if(parallax3){
-parallax3.style.backgroundAttachment='scroll';
-}
 }else{
-let parallax=document.getElementById('parallax');
-let parallax2=document.getElementById('parallax2');
-let parallax3=document.getElementById('parallax3');
 if(parallax){
+parallax.style.backgroundAttachment='fixed';
 parallax.style.backgroundPositionY=String(-0.5*window.pageYOffset)+'px';
 }
 if(parallax2){
+parallax2.style.backgroundAttachment='fixed';
 parallax2.style.backgroundPositionY=String(-0.5*window.pageYOffset+500)+'px';
 }
 if(parallax3){
+parallax3.style.backgroundAttachment='fixed';
 parallax3.style.backgroundPositionY=String(-0.5*window.pageYOffset+900)+'px';
 }
 }
