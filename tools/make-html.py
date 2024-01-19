f2283dc8af7d6fe84867162e17fc655f4e68de45
Mineyuki Iwasaki <mineyuki.iwasaki@gmail.com>
2024-01-19 23:02:08 +0700

    [Python] Fixed regular expression issue for make-html.

--- tools/make-html.py
+++ tools/make-html.py
@@ -19,9 +19,9 @@ base_html = f.read()
 f.close()
 
 # Get <head>, <header> and <footer> of base html
-base_head = re.search('<head>.*?<\/head>', base_html, flags=re.DOTALL).group(0)
-base_header = re.search('<header>.*?<\/header>', base_html, flags=re.DOTALL).group(0)
-base_footer = re.search('<footer>.*?<\/footer>', base_html, flags=re.DOTALL).group(0)
+base_head = re.search('<head>.*?</head>', base_html, flags=re.DOTALL).group(0)
+base_header = re.search('<header>.*?</header>', base_html, flags=re.DOTALL).group(0)
+base_footer = re.search('<footer>.*?</footer>', base_html, flags=re.DOTALL).group(0)
 
 # Find all html
 for dirpath, dirnames, filenames in os.walk(SOURCE_PATH):
@@ -36,19 +36,19 @@ for dirpath, dirnames, filenames in os.walk(SOURCE_PATH):
         f.close()
 
         # Get variables from html
-        color = re.search('(<color>)(\s*)(.*?)(\s*)(<\/color>)', html, flags=re.DOTALL).group(3)
-        description = re.search('(<description>)(\s*)(.*?)(\s*)(<\/description>)', html, flags=re.DOTALL).group(3)
-        icon = re.search('(<icon>)(\s*)(.*?)(\s*)(<\/icon>)', html, flags=re.DOTALL).group(3)
-        image = re.search('(<image>)(\s*)(.*?)(\s*)(<\/image>)', html, flags=re.DOTALL).group(3)
-        parallax = re.search('(<parallax>)(\s*)(.*?)(\s*)(<\/parallax>)', html, flags=re.DOTALL).group(3)
-        title = re.search('(<title>)(\s*)(.*?)(\s*)(<\/title>)', html, flags=re.DOTALL).group(3)
-        url = re.search('(<url>)(\s*)(.*?)(\s*)(<\/url>)', html, flags=re.DOTALL).group(3)
+        color = re.search('<color> *(.*?) *</color>', html, flags=re.DOTALL).group(1)
+        description = re.search('<description> *(.*?) *</description>', html, flags=re.DOTALL).group(1)
+        icon = re.search('<icon> *(.*?) *</icon>', html, flags=re.DOTALL).group(1)
+        image = re.search('<image> *(.*?) *</image>', html, flags=re.DOTALL).group(1)
+        parallax = re.search('<parallax> *(.*?) *</parallax>', html, flags=re.DOTALL).group(1)
+        title = re.search('<title> *(.*?) *</title>', html, flags=re.DOTALL).group(1)
+        url = re.search('<url> *(.*?) *</url>', html, flags=re.DOTALL).group(1)
         year = str(datetime.datetime.now().year)
 
         # Replace html with <head>, <header> and <footer> of base html
-        html = re.sub('<head>.*?<\/head>', base_head, html, flags=re.DOTALL)
-        html = re.sub('<header>.*?<\/header>', base_header, html, flags=re.DOTALL)
-        html = re.sub('<footer>.*?<\/footer>', base_footer, html, flags=re.DOTALL)
+        html = re.sub('<head>.*?</head>', base_head, html, flags=re.DOTALL)
+        html = re.sub('<header>.*?</header>', base_header, html, flags=re.DOTALL)
+        html = re.sub('<footer>.*?</footer>', base_footer, html, flags=re.DOTALL)
 
         # Replace html with variables
         html = re.sub('__COLOR__', color, html)
@@ -62,26 +62,26 @@ for dirpath, dirnames, filenames in os.walk(SOURCE_PATH):
             html = re.sub('__PARALLAX__', '', html)
         else:
             html = re.sub('__PARALLAX__', 'color-header', html)
-            html = re.sub('<script.*?javascript\.js.*?<\/script>', '', html, flags=re.DOTALL)
+            html = re.sub('<script.*?javascript.js.*?</script>', '', html, flags=re.DOTALL)
 
         # Setup for local environment if argv has 'local'
         if len(sys.argv) == 2 and sys.argv[1] == 'local':
-            html = re.sub('(https:\/\/scidoggames\.com.*?\/)\"', '\1index.html"', html)
+            html = re.sub('(https://scidoggames.com.*?/)\"', '\1index.html"', html)
             if os.name == 'nt':
-                html = re.sub('https:\/\/scidoggames\.com', LOCAL_DEPLOY_PATH_WINDOWS, html)
+                html = re.sub('https://scidoggames.com', LOCAL_DEPLOY_PATH_WINDOWS, html)
             else:
-                html = re.sub('https:\/\/scidoggames\.com', LOCAL_DEPLOY_PATH, html)
+                html = re.sub('https://scidoggames.com', LOCAL_DEPLOY_PATH, html)
 
         # Make new path
-        new_path = re.search('(https:\/\/scidoggames\.com\/)(.*)', url).group(2)
+        new_path = re.search('https://scidoggames.com/(.*)', url).group(1)
 
         # Add 'index.html' to new path if not contain '.html'
-        m = re.search('(.*)\.html', new_path)
+        m = re.search('.*.html', new_path)
         if not m:
             new_path = new_path + 'index.html'
 
         # Make directory if not exist
-        m = re.search('(.*)\/', new_path)
+        m = re.search('(.*)/', new_path)
         if m:
             new_dirpath = DEPLOY_PATH + '/' + m.group(1)
             if not os.path.exists(new_dirpath):
