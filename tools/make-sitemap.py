5f7d99d0de64f0c7f84eb8d34f5ce2131d50a714
Mineyuki Iwasaki <mineyuki.iwasaki@gmail.com>
2024-03-29 00:38:51 +0700

    [Tool] Refactored.

--- tools/make-sitemap.py
+++ tools/make-sitemap.py
@@ -5,35 +5,35 @@
 import os
 import re
 
-SITEMAP_PATH = os.path.join("docs", "sitemap.xml")
-SOURCE_PATH = "html"
+SITEMAP_PATH = os.path.join('docs', 'sitemap.xml')
+SOURCE_PATH = 'html'
 
 # Open sitemap
-f = open(SITEMAP_PATH, "w", encoding="utf-8")
-f.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
-f.write("<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n")
+f = open(SITEMAP_PATH, 'w', encoding='utf-8')
+f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+f.write('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n')
 
 # Find all html
 for root, _, files in os.walk(SOURCE_PATH):
     for file in files:
         base, ext = os.path.splitext(file)
-        if ext != ".html" or file == "base.html":
+        if ext != '.html' or file == 'base.html':
             continue
 
         # Read html
-        h = open(os.path.join(root, file), "r", encoding="utf-8")
+        h = open(os.path.join(root, file), 'r', encoding='utf-8')
         html = h.read()
         h.close()
 
         # Get url from html
-        url = re.search(r"<url>\s*(.*?)\s*</url>", html, flags=re.DOTALL).group(1)
+        url = re.search(r'<url>\s*(.*?)\s*</url>', html, flags=re.DOTALL).group(1)
 
         # Write url to sitemap
-        f.write("    <url>\n")
-        f.write(f"        <loc>{url}</loc>\n")
-        f.write("    </url>\n")
+        f.write('    <url>\n')
+        f.write(f'        <loc>{url}</loc>\n')
+        f.write('    </url>\n')
 
 # Close sitemap
-f.write("</urlset>\n")
+f.write('</urlset>\n')
 f.close()
-print(f"Generated {SITEMAP_PATH}")
+print(f'Generated {SITEMAP_PATH}')
--- tools/optimize-html.py
+++ tools/optimize-html.py
@@ -5,119 +5,119 @@
 import os
 import re
 
-DEPLOY_PATH = "docs"
+DEPLOY_PATH = 'docs'
 
 # Find all html, css, js, xml and svg
 for root, _, files in os.walk(DEPLOY_PATH):
     for file in files:
         base, ext = os.path.splitext(file)
-        if ext != ".html" and ext != ".css" and ext != ".js" and ext != ".xml" and ext != ".svg":
+        if ext != '.html' and ext != '.css' and ext != '.js' and ext != '.xml' and ext != '.svg':
             continue
 
         # Read html, css, js, xml or svg
         file_path = os.path.join(root, file)
-        f = open(file_path, "r", encoding="utf-8")
+        f = open(file_path, 'r', encoding='utf-8')
         original_content = f.read()
         f.close()
 
         # Optimize html or xml
-        if ext == ".html" or ext == ".xml":
+        if ext == '.html' or ext == '.xml':
             # Remove comments
-            content = re.sub(r"<!\-\-.*?\-\->", "", original_content, flags=re.DOTALL)
+            content = re.sub(r'<!\-\-.*?\-\->', '', original_content, flags=re.DOTALL)
             # Remove spaces
-            content = re.sub(r" +", " ", content)
-            content = re.sub(r"^\s+", "", content, flags=re.MULTILINE)
+            content = re.sub(r' +', ' ', content)
+            content = re.sub(r'^\s+', '', content, flags=re.MULTILINE)
 
         # Optimize css
-        if ext == ".css":
+        if ext == '.css':
             # Remove comments
-            content = re.sub(r"\/\*.*?\*\/", "", original_content, flags=re.DOTALL)
+            content = re.sub(r'\/\*.*?\*\/', '', original_content, flags=re.DOTALL)
             # Remove spaces
-            content = re.sub(r" +", " ", content)
-            content = re.sub(r"^\s+", "", content, flags=re.MULTILINE)
-            content = re.sub(r"\s*\+\s*", "+", content)
-            content = re.sub(r"\s*\-\s*", "-", content)
-            content = re.sub(r"\s*\*\s*", "*", content)
-            content = re.sub(r"\s*\/\s*", "/", content)
-            content = re.sub(r"\s*,\s*", ",", content)
-            content = re.sub(r"\s*:\s*", ":", content)
-            content = re.sub(r"\s*;\s*", ";", content)
-            content = re.sub(r"\s*\(\s*", "(", content)
-            content = re.sub(r"\s*\)\s*", ")", content)
-            content = re.sub(r"\s*\{\s*", "{", content)
-            content = re.sub(r"\s*\} *", "}", content)
+            content = re.sub(r' +', ' ', content)
+            content = re.sub(r'^\s+', '', content, flags=re.MULTILINE)
+            content = re.sub(r'\s*\+\s*', '+', content)
+            content = re.sub(r'\s*\-\s*', '-', content)
+            content = re.sub(r'\s*\*\s*', '*', content)
+            content = re.sub(r'\s*\/\s*', '/', content)
+            content = re.sub(r'\s*,\s*', ',', content)
+            content = re.sub(r'\s*:\s*', ':', content)
+            content = re.sub(r'\s*;\s*', ';', content)
+            content = re.sub(r'\s*\(\s*', '(', content)
+            content = re.sub(r'\s*\)\s*', ')', content)
+            content = re.sub(r'\s*\{\s*', '{', content)
+            content = re.sub(r'\s*\} *', '}', content)
 
         # Optimize js
-        if ext == ".js":
+        if ext == '.js':
             # Remove comments
-            content = re.sub(r"\/\*.*?\*\/", "", original_content, flags=re.DOTALL)
-            content = re.sub(r"\/\/.*$", "", content, flags=re.MULTILINE)
+            content = re.sub(r'\/\*.*?\*\/', '', original_content, flags=re.DOTALL)
+            content = re.sub(r'\/\/.*$', '', content, flags=re.MULTILINE)
             # Remove spaces
-            content = re.sub(r" +", " ", content)
-            content = re.sub(r"^\s+", "", content, flags=re.MULTILINE)
-            content = re.sub(r"\s*=\s*", "=", content)
-            content = re.sub(r"\s*<\s*", "<", content)
-            content = re.sub(r"\s*>\s*", ">", content)
-            content = re.sub(r"\s*\+\s*", "+", content)
-            content = re.sub(r"\s*\-\s*", "-", content)
-            content = re.sub(r"\s*\*\s*", "*", content)
-            content = re.sub(r"\s*\/\s*", "/", content)
-            content = re.sub(r"\s*,\s*", ",", content)
-            content = re.sub(r"\s*; *", ";", content)
-            content = re.sub(r"\s*\(\s*", "(", content)
-            content = re.sub(r"\s*\)\s*", ")", content)
-            content = re.sub(r" *\{ *", "{", content)
-            content = re.sub(r" *\} *", "}", content)
+            content = re.sub(r' +', ' ', content)
+            content = re.sub(r'^\s+', '', content, flags=re.MULTILINE)
+            content = re.sub(r'\s*=\s*', '=', content)
+            content = re.sub(r'\s*<\s*', '<', content)
+            content = re.sub(r'\s*>\s*', '>', content)
+            content = re.sub(r'\s*\+\s*', '+', content)
+            content = re.sub(r'\s*\-\s*', '-', content)
+            content = re.sub(r'\s*\*\s*', '*', content)
+            content = re.sub(r'\s*\/\s*', '/', content)
+            content = re.sub(r'\s*,\s*', ',', content)
+            content = re.sub(r'\s*; *', ';', content)
+            content = re.sub(r'\s*\(\s*', '(', content)
+            content = re.sub(r'\s*\)\s*', ')', content)
+            content = re.sub(r' *\{ *', '{', content)
+            content = re.sub(r' *\} *', '}', content)
 
         # Optimize svg
-        if ext == ".svg":
+        if ext == '.svg':
             # Remove unused elements
-            content = re.sub(r"<\?xml.*?\?>", "", original_content, flags=re.DOTALL)
-            content = re.sub(r"<defs.*?\/>", "", content, flags=re.DOTALL)
-            content = re.sub(r"<metadata.*?<\/metadata>", "", content, flags=re.DOTALL)
-            content = re.sub(r"<sodipodi:namedview.*?<\/sodipodi:namedview>", "", content, flags=re.DOTALL)
-            content = re.sub(r"<title>.*?<\/title>", "", content, flags=re.DOTALL)
-            content = re.sub(r"^\s*id=\".*?\"", "", content, flags=re.MULTILINE)
-            content = re.sub(r"^\s*inkscape:connector-curvature=\".*?\"", "", content, flags=re.MULTILINE)
-            content = re.sub(r"^\s*inkscape:export-filename=\".*?\"", "", content, flags=re.MULTILINE)
-            content = re.sub(r"^\s*inkscape:export-xdpi=\".*?\"", "", content, flags=re.MULTILINE)
-            content = re.sub(r"^\s*inkscape:export-ydpi=\".*?\"", "", content, flags=re.MULTILINE)
-            content = re.sub(r"^\s*inkscape:groupmode=\".*?\"", "", content, flags=re.MULTILINE)
-            content = re.sub(r"^\s*inkscape:label=\".*?\"", "", content, flags=re.MULTILINE)
-            content = re.sub(r"^\s*inkscape:version=\".*?\"", "", content, flags=re.MULTILINE)
-            content = re.sub(r"^\s*sodipodi:docname=\".*?\"", "", content, flags=re.MULTILINE)
-            content = re.sub(r"^\s*sodipodi:nodetypes=\".*?\"", "", content, flags=re.MULTILINE)
-            content = re.sub(r"^\s*version=\".*?\"", "", content, flags=re.MULTILINE)
-            content = re.sub(r"^\s*xmlns:cc=\".*?\"", "", content, flags=re.MULTILINE)
-            content = re.sub(r"^\s*xmlns:dc=\".*?\"", "", content, flags=re.MULTILINE)
-            content = re.sub(r"^\s*xmlns:inkscape=\".*?\"", "", content, flags=re.MULTILINE)
-            content = re.sub(r"^\s*xmlns:rdf=\".*?\"", "", content, flags=re.MULTILINE)
-            content = re.sub(r"^\s*xmlns:sodipodi=\".*?\"", "", content, flags=re.MULTILINE)
-            content = re.sub(r"^\s*xmlns:svg=\".*?\"", "", content, flags=re.MULTILINE)
+            content = re.sub(r'<\?xml.*?\?>', '', original_content, flags=re.DOTALL)
+            content = re.sub(r'<defs.*?\/>', '', content, flags=re.DOTALL)
+            content = re.sub(r'<metadata.*?<\/metadata>', '', content, flags=re.DOTALL)
+            content = re.sub(r'<sodipodi:namedview.*?<\/sodipodi:namedview>', '', content, flags=re.DOTALL)
+            content = re.sub(r'<title>.*?<\/title>', '', content, flags=re.DOTALL)
+            content = re.sub(r'^\s*id=".*?"', '', content, flags=re.MULTILINE)
+            content = re.sub(r'^\s*inkscape:connector-curvature=".*?"', '', content, flags=re.MULTILINE)
+            content = re.sub(r'^\s*inkscape:export-filename=".*?"', '', content, flags=re.MULTILINE)
+            content = re.sub(r'^\s*inkscape:export-xdpi=".*?"', '', content, flags=re.MULTILINE)
+            content = re.sub(r'^\s*inkscape:export-ydpi=".*?"', '', content, flags=re.MULTILINE)
+            content = re.sub(r'^\s*inkscape:groupmode=".*?"', '', content, flags=re.MULTILINE)
+            content = re.sub(r'^\s*inkscape:label=".*?"', '', content, flags=re.MULTILINE)
+            content = re.sub(r'^\s*inkscape:version=".*?"', '', content, flags=re.MULTILINE)
+            content = re.sub(r'^\s*sodipodi:docname=".*?"', '', content, flags=re.MULTILINE)
+            content = re.sub(r'^\s*sodipodi:nodetypes=".*?"', '', content, flags=re.MULTILINE)
+            content = re.sub(r'^\s*version=".*?"', '', content, flags=re.MULTILINE)
+            content = re.sub(r'^\s*xmlns:cc=".*?"', '', content, flags=re.MULTILINE)
+            content = re.sub(r'^\s*xmlns:dc=".*?"', '', content, flags=re.MULTILINE)
+            content = re.sub(r'^\s*xmlns:inkscape=".*?"', '', content, flags=re.MULTILINE)
+            content = re.sub(r'^\s*xmlns:rdf=".*?"', '', content, flags=re.MULTILINE)
+            content = re.sub(r'^\s*xmlns:sodipodi=".*?"', '', content, flags=re.MULTILINE)
+            content = re.sub(r'^\s*xmlns:svg=".*?"', '', content, flags=re.MULTILINE)
             # Remove spaces
-            content = re.sub(r"\n", " ", content)
-            content = re.sub(r"<", "\n<", content)
-            content = re.sub(r">", ">\n", content)
-            content = re.sub(r"\s*> *", ">", content)
-            content = re.sub(r"\s*\/> *", "/>", content)
-            content = re.sub(r" +", " ", content)
-            content = re.sub(r"^\s+", "", content, flags=re.MULTILINE)
+            content = re.sub(r'\n', ' ', content)
+            content = re.sub(r'<', '\n<', content)
+            content = re.sub(r'>', '>\n', content)
+            content = re.sub(r'\s*> *', '>', content)
+            content = re.sub(r'\s*\/> *', '/>', content)
+            content = re.sub(r' +', ' ', content)
+            content = re.sub(r'^\s+', '', content, flags=re.MULTILINE)
 
         # Write html, css, js, xml or svg
-        f = open(file_path, "w", encoding="utf-8")
+        f = open(file_path, 'w', encoding='utf-8')
         f.write(content)
         f.close()
-        print(f"Optimized {file_path} {len(original_content)} byte -> {len(content)} byte" + \
-            f" ({100 * len(content) / len(original_content):.2f} %)")
+        print(f'Optimized {file_path} {len(original_content)} byte -> {len(content)} byte' + \
+            f' ({100 * len(content) / len(original_content):.2f} %)')
 
 # Find all jpg or png
 for root, _, files in os.walk(DEPLOY_PATH):
     for file in files:
         base, ext = os.path.splitext(file)
-        if ext != ".jpg" and ext != ".png":
+        if ext != '.jpg' and ext != '.png':
             continue
 
         # Remove jpg or png
         file_path = os.path.join(root, file)
         os.remove(file_path)
-        print(f"Removed {file_path}")
+        print(f'Removed {file_path}')
