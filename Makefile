98415ae57d33e3c53e75aa40ac9080d7b41cdafd
Mineyuki Iwasaki <mineyuki.iwasaki@gmail.com>
2024-03-28 14:46:15 +0700

    [Tool] Refactored.

--- Makefile
+++ Makefile
@@ -1,32 +1,31 @@
 # (c) 2021 Mineyuki Iwasaki
 
-.PHONY: all clean local
-
 DEPLOY_PATH = docs
 
 all:
 	@make clean
-	@cp -fr css $(DEPLOY_PATH)/
-	@cp -fr images $(DEPLOY_PATH)/
-	@cp -fr js $(DEPLOY_PATH)/
-	@cp -fr press-kits $(DEPLOY_PATH)/
-	@cp -f etc/app-ads.txt $(DEPLOY_PATH)/
-	@cp -f etc/CNAME $(DEPLOY_PATH)/
+	@mkdir -p $(DEPLOY_PATH)
+	@cp -fr css $(DEPLOY_PATH)
+	@cp -fr images $(DEPLOY_PATH)
+	@cp -fr js $(DEPLOY_PATH)
+	@cp -fr press-kits $(DEPLOY_PATH)
+	@cp -f etc/app-ads.txt $(DEPLOY_PATH)
+	@cp -f etc/CNAME $(DEPLOY_PATH)
 	@tools/make-html.py
 	@tools/make-sitemap.py
 	@tools/optimize-html.py
 
-clean:
-	@rm -fr $(DEPLOY_PATH)/*
-
 local:
 	@make clean
-	@cp -fr css $(DEPLOY_PATH)/
-	@cp -fr images $(DEPLOY_PATH)/
-	@cp -fr js $(DEPLOY_PATH)/
-	@cp -fr press-kits $(DEPLOY_PATH)/
-	@cp -f etc/app-ads.txt $(DEPLOY_PATH)/
-	@cp -f etc/CNAME $(DEPLOY_PATH)/
+	@cp -fr css $(DEPLOY_PATH)
+	@cp -fr images $(DEPLOY_PATH)
+	@cp -fr js $(DEPLOY_PATH)
+	@cp -fr press-kits $(DEPLOY_PATH)
+	@cp -f etc/app-ads.txt $(DEPLOY_PATH)
+	@cp -f etc/CNAME $(DEPLOY_PATH)
 	@tools/make-html.py local
 	@tools/make-sitemap.py
 	@tools/optimize-html.py
+
+clean:
+	@rm -fr $(DEPLOY_PATH)
--- tools/make-sitemap.py
+++ tools/make-sitemap.py
@@ -5,37 +5,35 @@
 import os
 import re
 
-BASE_HTML_FILENAME = 'base.html'
-DEPLOY_PATH = 'docs'
-SITEMAP_FILENAME = 'sitemap.xml'
-SOURCE_PATH = 'html'
+SITEMAP_PATH = os.path.join("docs", "sitemap.xml")
+SOURCE_PATH = "html"
 
 # Open sitemap
-f = open(DEPLOY_PATH + '/' + SITEMAP_FILENAME, 'w', encoding='utf-8')
-f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-f.write('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n')
+f = open(SITEMAP_PATH, "w", encoding="utf-8")
+f.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+f.write("<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n")
 
 # Find all html
-for dirpath, dirnames, filenames in os.walk(SOURCE_PATH):
-    for filename in filenames:
-        base, ext = os.path.splitext(filename)
-        if ext != '.html' or filename == BASE_HTML_FILENAME:
+for root, _, files in os.walk(SOURCE_PATH):
+    for file in files:
+        base, ext = os.path.splitext(file)
+        if ext != ".html" or file == "base.html":
             continue
 
         # Read html
-        h = open(dirpath + '/' + filename, 'r', encoding='utf-8')
+        h = open(os.path.join(root, file), "r", encoding="utf-8")
         html = h.read()
         h.close()
 
         # Get url from html
-        url = re.search(r'<url>\s*(.*?)\s*</url>', html, flags=re.DOTALL).group(1)
+        url = re.search(r"<url>\s*(.*?)\s*</url>", html, flags=re.DOTALL).group(1)
 
         # Write url to sitemap
-        f.write('    <url>\n')
-        f.write('        <loc>' + url + '</loc>\n')
-        f.write('    </url>\n')
+        f.write("    <url>\n")
+        f.write(f"        <loc>{url}</loc>\n")
+        f.write("    </url>\n")
 
 # Close sitemap
-f.write('</urlset>\n')
+f.write("</urlset>\n")
 f.close()
-print(os.path.basename(__file__) + ': Generated ' + DEPLOY_PATH + '/' + SITEMAP_FILENAME + '.')
+print(f"Generated {SITEMAP_PATH}")
