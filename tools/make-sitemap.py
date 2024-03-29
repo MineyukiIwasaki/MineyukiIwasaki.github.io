d1b2933bdee30bcc4f0dc19a57628c5e8bc6a4a3
Mineyuki Iwasaki <mineyuki.iwasaki@gmail.com>
2024-03-29 15:19:00 +0700

    [Tool] Refactored make-sitemap.

--- tools/make-sitemap.py
+++ tools/make-sitemap.py
@@ -9,32 +9,27 @@ import sys
 SITEMAP_PATH = os.path.join(sys.argv[1], 'sitemap.xml')
 SOURCE_PATH = 'html'
 
-# Open sitemap
-f = open(SITEMAP_PATH, 'w', encoding='utf-8')
-f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-f.write('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n')
-
-# Find all html
+# Find all URLs
+urls = []
 for root, _, files in os.walk(SOURCE_PATH):
     for file in files:
         base, ext = os.path.splitext(file)
         if ext != '.html' or file == 'base.html':
             continue
+        f = open(os.path.join(root, file), 'r', encoding='utf-8')
+        html = f.read()
+        f.close()
+        urls.append(re.search(r'<url>\s*(.*?)\s*</url>', html, flags=re.DOTALL).group(1))
+urls.sort()
 
-        # Read html
-        h = open(os.path.join(root, file), 'r', encoding='utf-8')
-        html = h.read()
-        h.close()
-
-        # Get url from html
-        url = re.search(r'<url>\s*(.*?)\s*</url>', html, flags=re.DOTALL).group(1)
-
-        # Write url to sitemap
-        f.write('    <url>\n')
-        f.write(f'        <loc>{url}</loc>\n')
-        f.write('    </url>\n')
-
-# Close sitemap
+# Make sitemap
+f = open(SITEMAP_PATH, 'w', encoding='utf-8')
+f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+f.write('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n')
+for url in urls:
+    f.write('    <url>\n')
+    f.write(f'        <loc>{url}</loc>\n')
+    f.write('    </url>\n')
 f.write('</urlset>\n')
 f.close()
 print(f'Generated {SITEMAP_PATH}')
